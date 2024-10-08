d4ac1216f7172ddb1bc1d744fdea1f703b8b6fa1
Lorenz Mueller <lorenz.mueller@squer.at>
2024-10-08 17:52:35 +0200

    fix test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,13 +17,6 @@ describe('AppComponent', () => {
   it(`should have the 'testing-with-page-objects' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('testing-with-page-objects');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, testing-with-page-objects');
+    expect(app.title).toEqual('Loan Shark Banks');
   });
 });
